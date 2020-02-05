37e50e872ba8a8366df330fe21868fec31147aa5
zoubin0815 <2598730854@qq.com>
2020-02-05 23:47:39 +0800

    8.b

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 
 <template>
   <div id="app">
-   <el-button autofocus type="primary">按钮</el-button>
-   <el-button @click="handleClick" native-type="submit" type="primary">按钮</el-button>
-
+   <div>
+    <el-link href="https://element.eleme.io" target="_blank">默认链接</el-link>
+    <el-link type="primary">主要链接</el-link>
+    <el-link type="success">成功链接</el-link>
+    <el-link type="warning">警告链接</el-link>
+    <el-link type="danger">危险链接</el-link>
+    <el-link type="info">信息链接</el-link>
+  </div>
+<div>
+  <el-link disabled>默认链接</el-link>
+  <el-link type="primary" disabled>主要链接</el-link>
+  <el-link type="success" disabled>成功链接</el-link>
+  <el-link type="warning" disabled>警告链接</el-link>
+  <el-link type="danger" disabled>危险链接</el-link>
+  <el-link type="info" disabled>信息链接</el-link>
+</div>
+<div>
+  <el-link :underline="false">无下划线</el-link>
+  <el-link>有下划线</el-link>
+</div>
+<div>
+  <el-link icon="el-icon-edit">编辑</el-link>
+  <el-link>查看<i class="el-icon-view el-icon--right"></i> </el-link>
+</div>
     </div>
 </template>
 
